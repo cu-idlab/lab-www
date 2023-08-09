e8fa27b57467ad5bd72688d7b3c5d8f5f11b2425
whatknows <jedbrubaker@gmail.com>
2023-08-09 15:28:43 -0600

    Updated bash script

--- script/build.bash
+++ script/build.bash
@@ -3,14 +3,39 @@ echo " --- Move to the right directory. ---"
 # Make sure you're home
 cd ~
 
+# Clone the repository to a temporary folder and switch to the "build" branch:
+echo " --- Clone repo ---"
+git clone --depth 1 --branch build https://github.com/cu-idlab/lab-www.git ~/tmp/lab-www
+
+# Save the current commit hash to a file
+cd ~/tmp/lab-www
+last_commit_hash=$(git rev-parse HEAD)
+echo "$last_commit_hash" > ~/build_git_hash.txt
+
+## Check to see if the website has changed before continuing
+# Read the last commit hash from the file
+stored_commit_hash=$(cat ~/build_git_hash.txt)
+
+# Get the latest commit hash from the repository
+current_commit_hash=$(git rev-parse HEAD)
+
+# Compare the commit hashes
+if [ "$stored_commit_hash" != "$current_commit_hash" ]; then
+    # Proceed with the script
+    echo "Repository has changed. Proceeding with the update..."
+else
+    # Exit the script since there are no changes
+    echo "No changes in the repository. Exiting..."
+    exit
+fi
+
+# Update the stored commit hash
+echo "$current_commit_hash" > ~/build_git_hash.txt
+
 # Make a backup
 echo " --- Make a backup. ---"
 mkdir -p ~/backups/
 tar -czf ~/backups/idlab-$(date +%Y-%m-%d-%H-%M-%S).tar.gz /data/web/html/cmci.colorado.edu/idlab/
-
-# Clone the repository to a temporary folder and switch to the "build" branch:
-echo " --- Clone repo ---"
-git clone --depth 1 --branch build https://github.com/cu-idlab/lab-www.git ~/tmp/lab-www
 
 # Copy the files from the repository to the destination folder and exclude the .git directory:
 echo " --- Publish website ---"
